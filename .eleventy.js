aead1be4636171a9cf6cb4811c8ea290c68e1611
Brian Rem <brian.remavich@gmail.com>
2026-03-26 15:13:15 -0400

    move eleventy config to root

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 module.exports = function(eleventyConfig) {
+  
+  // Add getYear filter used in your templates
+  eleventyConfig.addFilter("getYear", (date) => {
+    const d = date ? new Date(date) : new Date();
+    return d.getFullYear();
+  });
+
   return {
     dir: {
       input: "src",
+      includes: "_includes",
       output: "_site"
-    }
+    },
+    passthroughFileCopy: true
   };
 };
